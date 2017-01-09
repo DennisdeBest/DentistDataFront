907e6a498f99e97f734856b8ab5a2560c1984b2b
DennisdeBest <dennis@debest.fr>
2017-01-09 10:24:48 +0100

    capistrano init

---
+++ config/deploy.rb
@@ -0,0 +1,71 @@
+# config valid only for current version of Capistrano
+lock "3.7.0"
+
+set :application, "dev.dentist-data.fr"
+set :repo_url, "git@github.com:DennisdeBest/DentistDataFront.git"
+
+set :linked_dirs, %w{node_modules app/bower_components}
+
+set :deploy_to, '/var/www/dev.dentist-data.fr'
+
+#set :npm_target_path, -> { release_path.join('subdir') } # default not set
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml", "config/secrets.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+set :default_env, { 
+  path: [
+    "#{shared_path}/node_modules/bower/bin", 
+    "#{shared_path}/node_modules/grunt-cli/bin",
+    "$PATH"].join(":")
+}
+
+
+
+namespace :deploy do
+  task :bower_and_npm_install do 
+    on roles(:app), in: :sequence, wait: 5 do
+      within release_path do 
+        execute :npm, "install"
+        execute :bower, "install"
+      end
+    end
+  end
+  after :published, :bower_and_npm_install
+end
+
+namespace :deploy do
+  task :build do 
+    on roles(:app), in: :sequence, wait: 5 do 
+      within release_path do 
+        execute :grunt, "build"
+      end
+    end
+  end
+  after :bower_and_npm_install, :build
+end
