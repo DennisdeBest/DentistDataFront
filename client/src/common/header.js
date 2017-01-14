71062399369734c598a6ef746effd0dc3afa602f
DennisdeBest <dennis@debest.fr>
2017-01-14 20:43:43 +0100

    Ajax request for login

--- client/src/common/header.js
+++ client/src/common/header.js
@@ -1,15 +1,29 @@
 (function() {
   'use strict';
 
-  function headerCtrl($scope, $log) {
+  function headerCtrl($scope, $log, $http) {
     $scope.showLoginPopin = false;
     $scope.createLoginPopin = function(){
       $scope.showLoginPopin = ! $scope.showLoginPopin;
       console.log("create login popin");
       $log.debug($scope.showLoginPopin);
     };
+    $scope.loginAction = function () {
+      $http({
+        method: 'POST',
+        url: 'http://devapi.dentist-data.fr/api/login',
+        data: {
+          username: $scope.loginUsername,
+          password: $scope.loginPassword
+        }
+      }).then(function successCallback(response){
+        $log.debug(response)
+      }, function errorCallback(response){
+        $log.debug(response)
+      })
+    }
   }
-  headerCtrl.$inject = ['$scope', '$log'];
+  headerCtrl.$inject = ['$scope', '$log', '$http'];
   angular.module('common.header', [])
       .controller('HeaderCtrl', headerCtrl);
 })();
