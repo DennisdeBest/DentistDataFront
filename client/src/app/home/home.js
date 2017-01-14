844ffe09e899f1cba4961ee6a572b97a3d010438
Unknown <thomas.dessombs@ynov.com>
2017-01-14 16:37:43 +0100

    Tosmah avance sur formulaire

--- client/src/app/home/home.js
+++ client/src/app/home/home.js
@@ -5,11 +5,49 @@
    * @name  HomeCtrl
    * @description Controller
    */
-  function HomeCtrl(data) {
+  function HomeCtrl($scope, data) {
     var home = this;
     home.data = data.data;
+    $scope.form = {};
+
+    $scope.submitForm = function () {
+      var datas = _getDatas();
+
+      console.log(datas);
+    };
+
+    function _getDatas () {
+      var form = $scope.form;
+
+      return {
+
+        C0ID : form.C0ID
+
+      }
+
+    }
   }
 
   angular.module('home', [])
     .controller('HomeCtrl', HomeCtrl);
 })();
+
+
+/*$scope.selected = {};
+
+ $scope.tab = [
+ { id: 0, name : "toto", datas : { id : 0, name : "tructoto", name2 : "truc2"}},
+ { id: 1, name : "bidule", datas : { id : 1, name : "trucbidul", name2 : "truc2"}},
+ { id: 2, name : "machin", datas : { id : 2, name : "trucmachin", name2 : "truc2"}},
+ { id: 3, name : "tutu", datas : { id : 3, name : "tructutu", name2 : "truc2"}}
+ ];
+
+ $scope.test = function(){
+ console.log();
+ };
+
+ <select ng-model="selected"
+ ng-options="el.name for el in tab">
+ <option value="" selected></option>
+ </select>
+ <button ng-click="test(selected)">button</button>*/
