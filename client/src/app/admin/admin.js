d0dce193728c507e4029f45941dc8b0fdb236830
DennisdeBest <dennis@debest.fr>
2017-01-16 12:34:46 +0100

    Get Error message on API login

--- client/src/app/admin/admin.js
+++ client/src/app/admin/admin.js
@@ -1,19 +1,36 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  /**
-   * @name  AdminCtrl
-   * @description Controller
-   */
-  function AdminCtrl($scope, $http, $log) {
-    var postObject = {};
-    postObject.test = "test";
-    $log.debug(postObject);
-    $http.post('http://devapi.dentist-data.fr/api/', JSON.stringify(postObject)).success(function (data) {
-      $log.debug(data);
-    });
-  }
-  //AdminCtrl.$inject = ['$scope', '$log', '$http', '$localStorage'];
-  angular.module('admin', [])
-    .controller('AdminCtrl', AdminCtrl);
+    /**
+     * @name  AdminCtrl
+     * @description Controller
+     */
+    function AdminCtrl($scope, $http, $log) {
+        $http.post('http://devapi.dentist-data.fr/api/auth', "").success(function (data, status, error) {
+            if (status == 200) {
+                var user;
+                if (data[0].username) {
+                    user = data[0]
+
+                    if (user) {
+                        $scope.username = user.username;
+                        $scope.email = user.email;
+                    }
+                }
+            }
+        }).error(function (error, status) {
+            $log.debug(error);
+            $log.debug(status);
+            $scope.errorMessage = error;
+        });
+
+
+
+        // if( data.roles.indexOf("ROLE_ADMIN") !== -1){
+        //  $scope.isAdmin = true;
+        //}
+    }
+
+    angular.module('admin', [])
+        .controller('AdminCtrl', AdminCtrl);
 })();
