220bc277ef67635113c494d6ff10fcd5104595bd
DennisdeBest <dennis@debest.fr>
2017-01-16 11:47:11 +0100

    Admin backoffice

--- client/src/app/admin/admin.js
+++ client/src/app/admin/admin.js
@@ -5,10 +5,15 @@
    * @name  AdminCtrl
    * @description Controller
    */
-  function AdminCtrl($scope, $http) {
-    $http.post
+  function AdminCtrl($scope, $http, $log) {
+    var postObject = {};
+    postObject.test = "test";
+    $log.debug(postObject);
+    $http.post('http://devapi.dentist-data.fr/api/', JSON.stringify(postObject)).success(function (data) {
+      $log.debug(data);
+    });
   }
-
+  //AdminCtrl.$inject = ['$scope', '$log', '$http', '$localStorage'];
   angular.module('admin', [])
     .controller('AdminCtrl', AdminCtrl);
 })();
