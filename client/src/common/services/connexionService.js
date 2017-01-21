9295dec1fb223e4805f7a191f8a76e2b458221b7
DennisdeBest <dennis@debest.fr>
2017-01-21 19:17:47 +0100

    getUser for dunkaka

--- client/src/common/services/connexionService.js
+++ client/src/common/services/connexionService.js
@@ -11,5 +11,29 @@ app.factory('connexionService', function () {
         getLogin: function () {
             return login;
         }
-    }
+    };
+    /*
+    this.getUser = function () {
+        var deferred = $q.defer();
+        $http.post('http://devapi.dentist-data.fr/api/auth', "").success(function (data, status) {
+            $log.debug(status);
+            if (status == 200) {
+                if (data[0].username) {
+                    //$log.debug(data);
+                    logged = true;
+                    deferred.resolve(data[0]);
+                    //return "kek";
+                    //return data[0];
+                }
+            } else {
+                deferred.reject();
+                $localStorage.$reset();
+            }
+        }).error(function (error, status) {
+            deferred.reject();
+        });
+
+        return deferred.promise;
+    };*/
+    //HTTP REQUEST FOR DUNKAKA
 });
