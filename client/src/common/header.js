91fe5270fc359e4b128d9d6f661940d7bb76d76c
DennisdeBest <dennis@debest.fr>
2017-01-21 19:12:06 +0100

    Fix develop

--- client/src/common/header.js
+++ client/src/common/header.js
@@ -1,7 +1,6 @@
 (function () {
     'use strict';
-
-    function headerCtrl($scope, $log, $http, $localStorage, connexionService) {
+    function headerCtrl($scope, $log, $http, $localStorage, $location, connexionService) {
         if ($localStorage.token) {
             $scope.logged = true;
         }
@@ -9,12 +8,12 @@
         $scope.login = connexionService.getLogin();
         $scope.swapped = function () {
             $scope.swap = connexionService.swapLogin();
-        }
+        };
         $scope.$watch(
             function () { return connexionService.getLogin(); },
             function (data) { $scope.login = data; }
         );
-    function headerCtrl($scope, $log, $http, $localStorage, $location, connexionService) {
+
 
         //Todo watchnot working on service value update
         /*$scope.watch(function () {
--- client/src/common/services/connexionService.js
+++ client/src/common/services/connexionService.js
@@ -1,43 +1,15 @@
 var app = angular.module('common.services.connexionService', []);
 
-app.factory('connexionService', function(){
+app.factory('connexionService', function () {
     var login = false;
 
     return {
-        swapLogin: function(){
+        swapLogin: function () {
             login = !login;
             console.log(login);
         },
-        getLogin: function(){
+        getLogin: function () {
             return login;
         }
-app.service('connexionService', function ($http, $log, $q) {
     }
-
-    this.getLogged = function () {
-        return logged;
-    };
-
-    this.getUser = function () {
-        var deferred = $q.defer();
-        $http.post('http://devapi.dentist-data.fr/api/auth', "").success(function (data, status) {
-            $log.debug(status);
-            if (status == 200) {
-                if (data[0].username) {
-                    //$log.debug(data);
-                    logged = true;
-                    deferred.resolve(data[0]);
-                    //return "kek";
-                    //return data[0];
-                }
-            } else {
-                deferred.reject();
-                $localStorage.$reset();
-            }
-        }).error(function (error, status) {
-            deferred.reject();
-        });
-
-        return deferred.promise;
-    };
 });
